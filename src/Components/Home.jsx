bbb495aa922d4e1eef9eb1a8232b386d86b86b5c
Nisha2703 <99647958+Nisha2703@users.noreply.github.com>
2025-01-14 06:14:51 +0000

    Updated App

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -27,12 +27,12 @@ const imageAltText = "Adult female in office setting leaning against a glass wal
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
-      <img className="background" src={image} alt="" />
+      <img className="background" src={image} alt="" style={{ filter: "blur(8px)"}}/>
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "70rem" }}>
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
-      <div style={{ position: "absolute", bottom: "3rem", left: "50%", filter: "blur(8px)" }}>
+      <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
         <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
       </div>
     </section>
